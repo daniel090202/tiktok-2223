cf71f1dc61a07875d19e2eda847e18d2546b9235
Daniel Nguyen <minhkhanh090202@gmail.com>
2023-08-22 14:13:27 +0700

    add-customize-cra-and-babel

--- src/App.js
+++ src/App.js
@@ -1,59 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
-import { useState } from "react";
-
-const courses = [
-  {
-    id: 1,
-    name: "HTML And CSS"
-  },
-  {
-    id: 2,
-    name: "JavaScript"
-  },
-  {
-    id: 3,
-    name: "PHP"
-  }
-]
+import '~/App.css';
 
 function App() {
-  const [checked, setChecked] = useState([])
-
-  function handleChecked(courseId) {
-    setChecked(prev => {
-      const isChecked = checked.includes(courseId)
-
-      if(isChecked) {
-        return checked.filter(id => id !== courseId)
-      }
-      else {
-        return [...prev, courseId]
-      }
-    })
-  }
-  
-  function handleSubmit() {
-    return
-  }
-
   return (
     <div className="App">
-      {courses.map(course => (
-        <div key={course.id}>
-          <input 
-            type="checkbox"
-            id={course.id}
-            checked={checked.includes(course.id)}
-            onChange={() => handleChecked(course.id)}
-          />
-          {course.name}
-        </div>
-      ))}
-
-      <button onClick={handleSubmit}>
-        Register
-      </button>
+      
     </div>
   );
 }
